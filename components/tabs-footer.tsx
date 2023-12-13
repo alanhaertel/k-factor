1c3521752c447518b9c884ed47f608e3d98288e2
alanhaertel <142280886+alanhaertel@users.noreply.github.com>
2023-12-12 22:20:00 -0300

    Added loading to the button
    231212

--- components/tabs-footer.tsx
+++ components/tabs-footer.tsx
@@ -7,6 +7,7 @@ import { Input } from './ui/input'
 import { InputLabel } from './ui/input-label'
 import { LinkedinIcon } from './ui/linkedin-icon'
 import { useCalculate } from '@/hooks/useCalculate'
+import { Loading } from './ui/loading'
 
 export function TabsFooter () {
     const { loading, totalK, leq, calculateK } = useCalculate()
@@ -18,7 +19,7 @@ export function TabsFooter () {
                     <Input value={leq} className='max-w-[10rem] h-7 dark:bg-slate-900 bg-gray-200 cursor-default focus-visible:ring-transparent' readOnly={true} type='number'/>
                     <InputLabel>&Sigma;k</InputLabel>
                     <Input value={totalK} className='max-w-[10rem] h-7 dark:bg-slate-900 bg-gray-200 cursor-default focus-visible:ring-transparent' readOnly={true} type='number'/>
-                    <Button type='submit' onClick={calculateK}> {loading ? 'Calculating...' : 'Calculate'}</Button>
+                    <Button type='submit' onClick={calculateK}> {loading ? <Loading /> : 'Calculate'}</Button>
                 </div>
                 <div className='px-2 flex items-center'>
                     <a href={'https://github.com/alanhaertel'} className={buttonVariants({ variant: 'ghost' })} target="_blank" rel="noreferrer">
