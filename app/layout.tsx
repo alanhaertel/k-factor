0de969713eec0efdb8a2d00ca9a1e169b9fa88fc
alanhaertel <142280886+alanhaertel@users.noreply.github.com>
2024-02-21 00:36:17 -0300

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout ({
                     <Toaster/>
                 </ThemeProvider>
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     )
